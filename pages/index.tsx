17f5804896082fd030f801dbc303ba2f4f8c2ce7
benlittlenz <benlittle53@gmail.com>
2021-10-16 20:09:22 +1300

    Converted project to typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
+import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Builder } from '../features/Builder/Builder'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,7 +16,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <Builder />
+
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
@@ -68,3 +68,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
