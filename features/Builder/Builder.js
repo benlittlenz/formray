a72888e0e1f8b8ea107222feca03fb9d051c8d93
benlittlenz <benlittle53@gmail.com>
2021-10-22 16:44:02 +1300

    Updating state to add new fields

--- features/Builder/Builder.js
+++ features/Builder/Builder.js
@@ -4,7 +4,7 @@ import {
   UserIcon,
   MenuAlt4Icon,
   MenuAlt2Icon,
-  LinkIcon
+  LinkIcon,
 } from "@heroicons/react/solid";
 import { v4 as uuidv4 } from "uuid";
 
@@ -13,21 +13,46 @@ import {
   TextAreaField,
   SelectField,
 } from "../../components/Fields";
-import { SelectMulti, SelectOne, Heading1Icon, Heading2Icon, Heading3Icon } from "./Icons";
+import {
+  SelectMulti,
+  SelectOne,
+  Heading1Icon,
+  Heading2Icon,
+  Heading3Icon,
+  ParagraphIcon,
+} from "./Icons";
 
 const icons = {
   name: <UserIcon className="h-6 w-6 text-gray-600" />,
   email: <AtSymbolIcon className="h-6 w-6 text-gray-600" />,
 };
 
-const initialState = [
-  {
-    component: "page",
-    label: "Page 1",
-    id: "0c946643-5a83-4545-baea-055b27b51e8a",
-    fields: [],
-  },
-];
+// const initialState = [
+//   {
+//     component: "page",
+//     label: "Page 1",
+//     id: "0c946643-5a83-4545-baea-055b27b51e8a",
+//     fields: [],
+//   },
+// ];
+const initialState = {
+  component: "page",
+  label: "Page 1",
+  id: "0c946643-5a83-4545-baea-055b27b51e8a",
+  fields: [
+    {
+      text_area: {
+        type: "textarea",
+        id: uuidv4(),
+        component: "text_area",
+        label: "",
+        hint: "",
+        placeholder: "",
+      },
+    },
+  ],
+};
+
 
 const fieldComponentLookup = {
   short_text: <InputField />,
@@ -65,28 +90,83 @@ export function Builder() {
     });
   };
 
+  const createElement = (element) => {
+    console.log("initialState", initialState);
+    setFormState((prevState) => {
+      return {
+        ...prevState,
+        fields: [...prevState.fields, formTypeLookup["short_text"]],
+      };
+    })
+    // const newArr = {
+    //   ...initialState,
+    //   fields: [...initialState.fields, formTypeLookup["short_text"]],
+    // };
+    // console.log("newArr", newArr);
+    // setFormState((prevState) => {
+    //   return {
+    //     ...prevState[0],
+    //   }
+    //   // let current = {
+    //   //   ...prevState,
+    //   // };
+    //   //   current[0].fields.push(formTypeLookup["short_text"])
+
+    //   //   return current
+    //   // current.prevState[0].fields.push(formTypeLookup["short_text"]);
+    //   // console.log("CURRENT", current);
+    // });
+  };
+  console.log("formState", formState);
   return (
     <div className="flex h-screen overflow-hidden bg-white rounded-md">
       <div className="hidden md:flex md:flex-shrink-0">
         <div className="flex flex-col w-96">
           <div className="flex flex-col flex-grow pt-5 overflow-y-auto  bg-white border-r border-gray-100 shadow-sm">
-            <div className="flex items-center px-4">
-              <div className="flex justify-center h-20 w-20 hover:bg-gray-100 text-center">
-                <div className="m-auto">
-                  <Heading1Icon />
-                  <p className="text-sm">Heading 1</p>
+            <div>
+              <h4 className="px-4 py-2 text-sm font-normal text-gray-500">
+                Content
+              </h4>
+              <div className="flex items-center px-4">
+                <div className="flex justify-center h-20 w-20 hover:bg-gray-100 hover:cursor-pointer text-center">
+                  <button
+                    type="button"
+                    className="m-auto"
+                    onClick={() => createElement("h1")}
+                  >
+                    <Heading1Icon />
+                    <p className="text-sm">Heading 1</p>
+                  </button>
                 </div>
-              </div>
-              <div className="flex justify-center h-20 w-20 hover:bg-gray-100 text-center">
-                <div className="m-auto">
-                  <Heading2Icon />
-                  <p className="text-sm">Heading 2</p>
+                <div className="flex justify-center h-20 w-20 hover:bg-gray-100 text-center">
+                  <button
+                    type="button"
+                    className="m-auto"
+                    onClick={() => createElement("h2")}
+                  >
+                    <Heading2Icon />
+                    <p className="text-sm">Heading 2</p>
+                  </button>
                 </div>
-              </div>
-              <div className="flex justify-center h-20 w-20 hover:bg-gray-100 text-center">
-                <div className="m-auto">
-                  <Heading3Icon />
-                  <p className="text-sm">Heading 3</p>
+                <div className="flex justify-center h-20 w-20 hover:bg-gray-100 text-center">
+                  <button
+                    type="button"
+                    className="m-auto"
+                    onClick={() => createElement("h3")}
+                  >
+                    <Heading3Icon />
+                    <p className="text-sm">Heading 3</p>
+                  </button>
+                </div>
+                <div className="flex justify-center h-20 w-20 hover:bg-gray-100 text-center">
+                  <button
+                    type="button"
+                    className="m-auto"
+                    onClick={() => createElement("p")}
+                  >
+                    <ParagraphIcon />
+                    <p className="text-sm">Paragraph</p>
+                  </button>
                 </div>
               </div>
             </div>
@@ -97,55 +177,87 @@ export function Builder() {
               </h4>
               <div className="px-4 flex items-center">
                 <div className="flex justify-center h-20 w-20 hover:bg-gray-100 text-center">
-                  <div className="m-auto">
+                  <button
+                    type="button"
+                    className="m-auto"
+                    onClick={() => createElement("user")}
+                  >
                     <UserIcon className="mx-auto h-6 w-6 text-gray-600" />
                     <p className="text-sm">User</p>
-                  </div>
+                  </button>
                 </div>
                 <div className="flex justify-center h-20 w-20 hover:bg-gray-100 text-center">
-                  <div className="m-auto">
+                  <button
+                    type="button"
+                    className="m-auto"
+                    onClick={() => createElement("email")}
+                  >
                     <AtSymbolIcon className="mx-auto h-6 w-6 text-gray-600" />
                     <p className="text-sm">Email</p>
-                  </div>
+                  </button>
                 </div>
                 <div className="flex justify-center h-20 w-20 hover:bg-gray-100 text-center">
-                  <div className="m-auto">
+                  <button
+                    type="button"
+                    className="m-auto"
+                    onClick={() => createElement("short_text")}
+                  >
                     <MenuAlt4Icon className="mx-auto h-6 w-6 text-gray-600" />
                     <p className="text-sm">Short Text</p>
-                  </div>
+                  </button>
                 </div>
                 <div className="flex justify-center h-20 w-20 hover:bg-gray-100 text-center">
-                  <div className="m-auto">
+                  <button
+                    type="button"
+                    className="m-auto"
+                    onClick={() => createElement("long_text")}
+                  >
                     <MenuAlt2Icon className="mx-auto h-6 w-6 text-gray-600" />
                     <p className="text-sm">Long Text</p>
-                  </div>
+                  </button>
                 </div>
               </div>
               <div className="mt-6">
                 <div className="px-4 flex items-center">
                   <div className="flex justify-center h-20 w-20 hover:bg-gray-100 text-center">
-                    <div className="m-auto">
+                    <button
+                      type="button"
+                      className="m-auto"
+                      onClick={() => createElement("number")}
+                    >
                       <LinkIcon className="mx-auto h-6 w-6 text-gray-600" />
                       <p className="text-sm">Number</p>
-                    </div>
+                    </button>
                   </div>
                   <div className="flex justify-center h-20 w-20 hover:bg-gray-100 text-center">
-                    <div className="m-auto">
+                    <button
+                      type="button"
+                      className="m-auto"
+                      onClick={() => createElement("url")}
+                    >
                       <LinkIcon className="mx-auto h-6 w-6 text-gray-600" />
                       <p className="text-sm">URL</p>
-                    </div>
+                    </button>
                   </div>
                   <div className="flex justify-center h-20 w-20 hover:bg-gray-100 text-center">
-                    <div className="m-auto">
+                    <button
+                      type="button"
+                      className="m-auto"
+                      onClick={() => createElement("select")}
+                    >
                       <SelectOne />
                       <p className="text-sm">Select</p>
-                    </div>
+                    </button>
                   </div>
                   <div className="flex justify-center h-20 w-20 hover:bg-gray-100 text-center">
-                    <div className="m-auto">
+                    <button
+                      type="button"
+                      className="m-auto"
+                      onClick={() => createElement("multi_select")}
+                    >
                       <SelectMulti />
                       <p className="text-sm">Multi-Select</p>
-                    </div>
+                    </button>
                   </div>
                 </div>
               </div>
@@ -153,6 +265,13 @@ export function Builder() {
           </div>
         </div>
       </div>
+      <ul>
+        {formState.fields.map((field) => {
+          return (
+            <li key={field.id}>{fieldComponentLookup[field.component]}</li>
+          );
+        })}{" "}
+      </ul>
     </div>
   );
 }
--- features/Builder/Icons/index.js
+++ features/Builder/Icons/index.js
@@ -3,3 +3,4 @@ export * from "./SelectOne"
 export * from "./Heading1Icon"
 export * from "./HeadingH2Icon"
 export * from "./HeadingH3Icon";
+export * from "./ParagraphIcon";
